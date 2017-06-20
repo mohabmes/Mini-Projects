cbb542ac5e2d732a8fa00ac4b70122bec8b83f30
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-06-20 11:12:50 +0200

    URLShortener: Initial Commit

---
+++ PHP/URL Shortener/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="css/global.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="title">Shorten a URL.</h1>
+
+      <form action="shorten.php" method="post">
+        <input type="url" name="url" placeholder="Enter a URL here">
+        <input type="submit" value="Shorten">
+      </form>
+    </div>
+  </body>
