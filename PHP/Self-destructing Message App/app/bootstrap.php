84f728fd5a7794e01f21ba73c761750fc97385d4
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-05 17:02:15 +0200

    MSG-App: DB conntection

--- PHP/Self-destructing Message App/app/bootstrap.php
+++ PHP/Self-destructing Message App/app/bootstrap.php
@@ -19,6 +19,16 @@ $container['view'] = function($container){
   return $view;
 };
 
+
+$container['db'] = function($container){
+  return new PDO(
+        'mysql:host=' . $container['config']->get('db.mysql.host') . 
+		';dbname=' . $container['config']->get('db.mysql.dbname'),
+        $container['config']->get('db.mysql.username')
+        //,$container['config']->get('db.mysql.password')
+    );
+};
+
 $app = new \Slim\App($container);
 
 require("routes.php");
--- PHP/Self-destructing Message App/app/routes.php
+++ PHP/Self-destructing Message App/app/routes.php
@@ -1,6 +1,24 @@
 <?php
 
 $app->get('/', function ($request, $response, $args){
-  //echo $this->config->get('db.mysql.dbname');
   $this->view->render($response, 'home.twig');
-});
+})->setName('home');
+
+$app->post('/post', function($request, $response, $args) {
+	
+  $params = $request->getParams();
+  
+  $hash = md5(uniqid(true));
+
+  $message = $this->db->prepare(
+	"INSERT INTO messages(hash, message) 
+	VALUES (:hash, :message)
+  ");
+  $message->execute([
+      'hash' => $hash,
+      'message' => $params['message']
+  ]);
+
+  return $response->withRedirect($this->router->pathFor('home'));
+
+})->setName('send');
