96dff039ee1864fb45f66c1476716370390edec9
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-07-07 23:03:31 +0200

    AuthenSys : Add Config Class

---
+++ PHP/Authentication System/classes/Config.php
@@ -0,0 +1,17 @@
+<?php
+class Config{
+  public static function get($path = null){
+    if($path){
+      $path = explode('/', $path);
+      $config = $GLOBALS['config'];
+
+      foreach ($path as $key) {
+        if(isset($config[$key])){
+          $config = $config[$key];
+        }
+      }
+      return $config;
+    }
+    return false;
+  }
+}AuthenSys
