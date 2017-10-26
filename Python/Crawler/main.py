edc6df34d1f4648bab0ff59dcdcf7cfa95b07a1a
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2017-10-26 15:39:09 +0200

    PyCrawler: Update File Naming

--- Python/Crawler/main.py
+++ Python/Crawler/main.py
@@ -1,46 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 
+baseurl=""
+craweled = set()
+
 def crawl(url, max_pages=10, recursive=0):
     page = 1
-    craweled = set()
 
-    while page <= max_pages:
-        try:
-            source_code = requests.get(url, allow_redirects=False)
-        except:
-            print("this URL ({}) is not accessible". format(url))
+    if not url in craweled:
+        while page <= max_pages:
+            try:
+                source_code = requests.get(url, allow_redirects=False)
+            except:
+                print("this URL ({}) is not accessible". format(url))
+                page += 1
+                continue
+            # just get the code
+            plain_text = source_code.text.encode('ascii', 'replace')
+            # BeautifulSoup objects
+            soup = BeautifulSoup(plain_text,'html.parser')
+            for link in soup.findAll('a'):
+                href = link.get('href')
+                title = link.string
+                # print(href + "\t" + str(title))
+                # print(link)
+                if not href[:4]==baseurl[:4]:
+                    if not baseurl.endswith("/"):
+                        href = baseurl + "/" + href
+                    href = baseurl + href
+                craweled.add(str(href))
             page += 1
-            continue
-        # just get the code
-        plain_text = source_code.text.encode('ascii', 'replace')
-        # BeautifulSoup objects
-        soup = BeautifulSoup(plain_text,'html.parser')
-        for link in soup.findAll('a'):
-            href = link.get('href')
-            title = link.string
-            # print(href + "\t" + str(title))
-            # print(link)
-            craweled.add(str(href))
-        page += 1
 
-    fw = open("ds.txt", "a")
-    for link in craweled:
-        print(link)
-        fw.write(link + "\n")
+        
+        filename = baseurl.replace(":","_").replace("/","_") + ".txt"
+        fw = open(filename, "w")
+        for link in craweled:
+            print(link)
+            fw.write(link + "\n")
 
-    if recursive is 1:
-        recursive_crawl(craweled)
+        if recursive is 1:
+            recursive_crawl(craweled)
 
-    fw.close()
+        fw.close()
 # end of crawl
 
 
 def recursive_crawl(list, r=0):
     for link in list:
-        print(link)
         crawl(link, recursive=r)
 # end of recursive_crawl
 
-
-crawl('https://www.youtube.com/', 10, 0)
+baseurl=input("URL: ")
+crawl(baseurl, int(input("crawl recursivly[1/0]: ")))
