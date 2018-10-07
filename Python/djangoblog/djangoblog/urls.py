f9187dc91e23c229d1c131c524ebb577a3bd5a5c
Mohab Elsheikh <mohab.elsheikh@gmail.com>
2018-10-07 18:06:37 +0200

    Update Blog

--- Python/djangoblog/djangoblog/urls.py
+++ Python/djangoblog/djangoblog/urls.py
@@ -5,12 +5,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import staticfiles
-
+from articles import views as article_view
 
 urlpatterns = [
 				path('admin/', admin.site.urls),
 				path('accounts/', include('accounts.urls')),
-				path('', views.index),
+				path('', article_view.article_list),
 				path('articles/', include('articles.urls')),
 
 ]
